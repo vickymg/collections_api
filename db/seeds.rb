3a9cb9725262c7855d02011d5bb5fd08e6951d4a
Vicky Gray <vickygray26@hotmail.com>
2016-09-10 15:57:11 +0100

    added db seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+collections = [["DVDs", "My DVD collection"], ["Books", "My Book collection"]]
+
+items_one = ["Ocean's Eleven", "Skyfall", "Frozen", "Sherlock"]
+
+items_two = ["Gone Girl", "To Kill a Mockingbird", "Life of Pi", "The Son"]
+
+collections.each do |name, description|
+  Collection.create!(name: name, description: description)
+end
+
+items_one.each do |name|
+  Item.create!(name: name, collection_id: 1)
+end
+
+items_two.each do |name|
+  Item.create!(name: name, collection_id: 2)
+end
